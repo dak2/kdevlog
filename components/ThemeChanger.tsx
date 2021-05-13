5f699207dbf864a2c6f8444ef4f3ae389b87712a
dak2 <dawt2h@gmail.com>
2021-05-13 22:52:13 +0900

    add color to icons

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -29,19 +29,20 @@ const ThemeChanger = () => {
 
   return (
     <div className="flex">
-      <div className="mr-2 text-xs">
+      <div className="mr-2 text-xs text-red-500 dark:text-white">
         <svg
-          className="w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          className="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
           <path
+            fill-rule="evenodd"
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
           />
         </svg>
       </div>
@@ -58,19 +59,18 @@ const ThemeChanger = () => {
           toggle
         </label>
       </div>
-      <div className="text-xs">
+      <div className="text-xs dark:text-yellow-200">
         <svg
-          className="w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          className="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
           />
         </svg>
       </div>
