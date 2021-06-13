5253c55cc8e1bdbc8ee52e39ef2ff557a90b4eec
dak2 <dawt2h@gmail.com>
2021-06-13 20:02:29 +0900

    fix file name

--- components/molecules/headerBar.tsx
+++ components/molecules/headerBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import GithubIcon from '../atoms/icons/github';
-import ThemeChanger from '../atoms/themeChanger';
+import ThemeChangeToggle from '../atoms/themeChanger';
 
 const HeaderBar = () => {
   return (
@@ -11,7 +11,7 @@ const HeaderBar = () => {
         </h1>
       </Link>
       <div className="flex ml-64">
-        <ThemeChanger />
+        <ThemeChangeToggle />
         <a href="https://github.com/dak2">
           <GithubIcon />
         </a>
