8c7ae456f7ab7cf920f436062ceec72e822f2056
dak2 <dawt2h@gmail.com>
2022-06-18 16:41:44 +0900

    add generic icon component

---
+++ components/atoms/genericIcon.tsx
@@ -0,0 +1,34 @@
+import { FaGithub, FaAws, FaDocker, FaChevronLeft, FaChevronRight, FaRust, FaNodeJs, FaReact } from 'react-icons/fa';
+import { SiJavascript, SiRuby, SiRubyonrails, SiTypescript, SiDeno } from 'react-icons/si';
+
+type Props = {
+  iconName: string;
+}
+
+const GenericIcon = (props: Props) => {
+  const iconName = props.iconName;
+
+  const icons = {
+    'aws': <FaAws/>,
+    'docker': <FaDocker/>,
+    'github': <FaGithub/>,
+    'javascript': <SiJavascript/>,
+    'typescript': <SiTypescript/>,
+    'nodejs': <FaNodeJs/>,
+    'react': <FaReact/>,
+    'ruby': <SiRuby/>,
+    'ruby-on-rails': <SiRubyonrails/>,
+    'rust': <FaRust/>,
+    'deno': <SiDeno/>,
+    'right': <FaChevronRight/>,
+    'left': <FaChevronLeft/>,
+  }
+
+  return (
+    <div className="w-8">
+      {icons[iconName]}
+    </div>
+  )
+}
+
+export default GenericIcon;
